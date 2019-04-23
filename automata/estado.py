15d1d981247c955f2598c0f2bbc744d0cefc3596
Robin <marzg.zl55@gmail.com>
2019-04-23 04:57:46 -0500

    clase automata

--- automata/estado.py
+++ automata/estado.py
@@ -1,13 +1,23 @@
 from grafo.nodo import Nodo, Arista
 class Estado(Nodo):
-    def __init__(self, valor):
+    
+    def __init__(self, valor, final):
         super().__init__(valor)
         self.__valor = self._Nodo__valor
         self.__transiciones = self._Nodo__aristas
         self.addTransicion = self.addArista
+        self.__final = final
+        self.__palabra = []
+        
+    
     def addArista(self, valor, destino):
         self._Nodo__aristas.append(Arista(valor, destino))
+    
     def getValor(self):
         return self.__valor
+    
     def getTransiciones(self):
         return self.__transiciones
+    
+    def esFinal(self):
+        return self.__final
